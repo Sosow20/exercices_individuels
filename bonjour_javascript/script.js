f1953dcadf0738a0360620546e07d897deffa281
Sosow20 <sofiacherifi1995@gmail.com>
2025-06-19 16:43:20 +0200

    first commit

---
+++ bonjour_javascript/script.js
@@ -0,0 +1,54 @@
+//Etape 2 : Premier Code
+//1st variable:
+
+// let message = "Bonjour !"
+// console.log(message)
+
+//2nd variable:
+
+// let firstname = "Beyonce"
+// message = "Bonjour" + " " + firstname + " " + "!"
+// console.log(message)
+
+//Etape 3: Dans une fonction
+
+// function sayHello(firstname) {
+
+//     message = "Bonjour" + " " + firstname + " " + "!"
+//     console.log(message)
+// }
+
+// sayHello()
+
+// 5.Corrige l'appel
+
+// function sayHello(firstname) {
+
+//      message = "Bonjour" + " " + firstname + " " + "!"
+//      console.log(message)
+// }
+// sayHello("Beyonce")
+
+// Etape 4 : un second paramètre
+
+// function sayHello(firstname, hour) {
+//     if (hour >= 18) {
+//         message = "Bonsoir" + " " + firstname + " " + "!"
+//         console.log(message)
+//     } else {
+//         message = "Bonjour" + " " + firstname + " " + "!"
+//         console.log(message)
+//     }
+    
+// }
+
+// sayHello("Beyonce", 11)
+// sayHello("Beyonce", 18)
+// sayHello("Beyonce", 17)
+
+
+// Etape 6 : Première manipulation du Dom
+
+// let firstname = "Beyonce"
+// message = "Bonjour" + " " + firstname + " " + "!"
+// document.querySelector('h1').innerText = message
