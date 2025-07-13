2ed6c7de0ca72e601ca020ec7a293ebf7733d337
Sosow20 <sofiacherifi1995@gmail.com>
2025-07-13 09:34:33 +0200

    parlez vous le morse 1

--- parlez_vous_le_morse/script.js
+++ parlez_vous_le_morse/script.js
@@ -1,3 +1,38 @@
-function getLatinCharacterList(){
-    
+import {latinToMorse} from "./latintomorse.js";
+import {morseToLatin} from "./morsetolatin.js";
+
+//^ Etape 1 :
+
+function getLatinCharacterList(text) {
+    return text.split("")                                //.split découpe le mot en plusieurs caractères
 }
+// console.log(getLatinCharacterList('hello, world'))
+
+//^ Etape 2 :
+
+function translateLatinCharacter(alphabet) {
+    const majAlphabet = alphabet.toUpperCase()         // on lui dit de convertir un caractère en Majuscule à l'inverse de .toLowerCase
+    return latinToMorse[majAlphabet]                   // on lui dit de chercher le caractère en Majuscule dans le tableau latinToMorse
+}
+
+// console.log(translateLatinCharacter('B'))
+
+
+//^ Etape 3
+
+function encode(text) {
+    const sentence = getLatinCharacterList(text)                            //on divise le text en plusieurs caractères
+    const encodeAlphabet = sentence.map(translateLatinCharacter)            // on parcours chaque caractère du tableau en lui attribuant son équivalent morse
+    return encodeAlphabet.join('')                                          // et enfin on rassemble les caractère morse 
+}
+
+console.log(encode('HELLO'))
+
+//^ Etape 4
+
+
+
+
+//^ Etape 5 DOM
+
+
